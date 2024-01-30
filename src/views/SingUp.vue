e8c73b57d5f038bc8ee6fdc50b326a22f4903d70
Angkana016 laraverl10-week9 <myemail@example.com>
2024-01-30 12:02:17 +0700

    first commit

---
+++ src/views/SingUp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+    <div class="row">
+    <div class="col-md mb-1">
+        xxx
+    </div>
+    <div class="col-md-7">
+        bbb   
+    </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
